112250d3f450124c907578182118868cb4d8a6fd
Sergio <sergio.jose.ruiz@est.fib.upc.edu>
2019-02-04 12:38:01 +0100

    Code cleaned

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ MONGODB_HOST = 'localhost'
 MONGODB_PORT = 27017
 DBS_NAME = 'donorschoos'
 COLLECTION_NAME = 'projects'
+
+# Here we say to Mongo to retrieve this values only
 FIELDS = {  'school_district': True,
             'school_magnet': True,
             'school_county': True,
@@ -18,7 +20,6 @@ FIELDS = {  'school_district': True,
             'total_price_excluding_optional_support': True,
             'school_nlns': True,
             'school_state': True, 
-            'resource_type': True, 
             'school_kipp': True, 
             'date_completed': True,
             'total_donations': True,
@@ -31,21 +32,9 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/api/data")
-def donorschoose_project():
-    connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
-    collection = connection[DBS_NAME][COLLECTION_NAME]
-    projects = collection.find(projection=FIELDS, limit=11000)
-    #projects = collection.find(projection=FIELDS)
-    json_projects = []
-    for project in projects:
-        json_projects.append(project)
-    json_projects = json.dumps(json_projects, default=json_util.default)
-    connection.close()
-    return json_projects
 
-@app.route("/donorschoose/projects")
-def donorschoose_projects():
+@app.route("/budget/projects")
+def budget_projects():
     connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
     collection = connection[DBS_NAME][COLLECTION_NAME]
     projects = collection.find(projection=FIELDS, limit=11000)
@@ -64,4 +53,4 @@ def static_file(path):
 
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1',port=5000,debug=True)
+    app.run(host='0.0.0.0',port=5000,debug=True)
--- static/js/Dashboard.js
+++ static/js/Dashboard.js
@@ -1,15 +1,15 @@
 queue()
-    .defer(d3.json, "/donorschoose/projects")
-    .defer(d3.json, "./static/geojson/us-states.json")
+	// Call this to access database
+    .defer(d3.json, "/budget/projects")
+    .defer(d3.json, "./static/geojson/ar-states.json")
     .await(makeGraphs);
 
+
 function makeGraphs(error, apiData, statesJson) {
-	
-//Start Transformations
+	//Start Transformations
 	var dataSet = apiData;
-	var dateFormat = d3.time.format("%m/%d/%Y");	
-	var xa = 0;
-	var ya = 0;
+	var dateFormat = d3.time.format("%m/%d/%Y");
+
 	dataSet.forEach(function(d) {
 		d.date_completed = dateFormat.parse(d.date_completed);
 		d.date_completed.setDate(1);
@@ -21,146 +21,55 @@ function makeGraphs(error, apiData, statesJson) {
 	//Create a Crossfilter instance
 	var ndx = crossfilter(dataSet);
 
-
 	//Define Dimensions
 	var datePosted = ndx.dimension(function(d) { return d.date_completed; });
-	var gradeLevel = ndx.dimension(function(d) { return d.grade_level; });
-	var resourceType = ndx.dimension(function(d) { return d.resource_type; });
 	var fundingStatus = ndx.dimension(function(d) { return d.funding_status; });
-	var povertyLevel = ndx.dimension(function(d) { return d.poverty_level; });
-	var totalDonations  = ndx.dimension(function(d) { return d.total_donations; });
 	var state = ndx.dimension(function(d) { return d.school_state; });
-
-
-
-
-///////////////////////////////////////////////////
-	var schoolDistrict = ndx.dimension(function(d) { return d.school_district; });
-	var schoolMetro = ndx.dimension(function(d) { return d.school_metro; });
-	var totalExcluding = ndx.dimension(function(d) { return d.total_price_excluding_optional_support; });
+	var type = ndx.dimension(function(d) { return d.school_district; });
 	var institution = ndx.dimension(function(d) { return d.school_nlns; });
-	var fuente  = ndx.dimension(function(d) { return d.school_magnet; });
-	var caracter  = ndx.dimension(function(d) { return d.school_year_round; });
-	var stateFull = ndx.dimension(function(d) { return d.school_city; });
-
+	var source  = ndx.dimension(function(d) { return d.school_magnet; });
 	var year = ndx.dimension(function(d) { return d.school_county; });
-///////////////////////////////////////////////////
+	var caracter  = ndx.dimension(function(d) { return d.school_year_round; });
 
 
-
-
-
-
-
-
-
-
-
-
+	// Create all filter
+	var all = ndx.groupAll();
 
 	//Calculate metrics
-	var projectsByDate = datePosted.group(); 
-	var projectsByGrade = gradeLevel.group(); 
-	var projectsByResourceType = resourceType.group();
-	var projectsByFundingStatus = fundingStatus.group();
-	var projectsByPovertyLevel = povertyLevel.group();
-	var stateGroup = state.group();
+	var groupByDate = datePosted.group(); 
+	var yearGroup = year.group(); 
+	var schoolDistrictGroup = type.group();
+	var sourceGroup = source.group();
+	var institutionGroup = institution.group();
+	var caracterGroup = caracter.group();
+
+
+	var budgetGroup = ndx.groupAll().reduceSum(function(d) {
+		return d.total_price_excluding_optional_support;
+	});
+	var budgetMoney = fundingStatus.group().reduceSum(function(d) {
+		return d.total_price_excluding_optional_support;
+	});
 	var totalDonationsByState = state.group().reduceSum(function(d) {
 		return d["total_donations"];
 	});
 
-
-///////////////////////////////////////////////////
-	var yearGroup = year.group(); 
-	var schoolDistrictGroup = schoolDistrict.group();
-	var schoolMetroGroup = schoolMetro.group();
-	var fuenteGroup = fuente.group();
-	var caracterGroup = caracter.group();
-	var stateFullGroup = stateFull.group();
-
-	//var totalExcludingGroup = totalExcluding.group();
-	var institutionGroup = institution.group();
-////////////////////////////////////////////////////
-
-
-
-
-
-
-
-
-
-
-	var all = ndx.groupAll();
-
-	//Calculate Groups
-	var totalDonationsState = state.group().reduceSum(function(d) {
-		return d.total_donations;
-	});
-
-	var totalDonationsGrade = gradeLevel.group().reduceSum(function(d) {
-		return d.grade_level;
-	});
-
-	var totalDonationsFundingStatus = schoolDistrictGroup.reduceSum(function(d) {
-		return d.funding_status;
-	});
-
-
-
-
-
-
-
-///////////////////////////////////////////////////
-	var presupuesto = fundingStatus.group().reduceSum(function(d) {
-		return d.total_price_excluding_optional_support;
-	});
-
-	var presupuestoBySchool = schoolDistrictGroup.reduceSum(function(d) {
-		return d.total_price_excluding_optional_support;
-	});
-	
-///////////////////////////////////////////////////
-
-
-
-
-
-
-	var presupuestoP = ndx.groupAll().reduceSum(function(d) {return d.total_price_excluding_optional_support;});
-	var netTotalDonations = ndx.groupAll().reduceSum(function(d) {return d.school_nlns;});
-
 	//Define threshold values for data
 	var minDate = datePosted.bottom(1)[0].date_completed;
 	var maxDate = datePosted.top(1)[0].date_completed;
-
 	var max_state = totalDonationsByState.top(1)[0].value;
 
-console.log(minDate);
-console.log(maxDate);
-
-    //Charts
+    //Initialize and map Charts
 	var dateChart = dc.lineChart("#date-chart");
-	var gradeLevelChart = dc.rowChart("#grade-chart");
-	var resourceTypeChart = dc.rowChart("#resource-chart");
-	var fundingStatusChart = dc.pieChart("#funding-chart");
-	var povertyLevelChart = dc.rowChart("#poverty-chart");
-	var totalProjects = dc.numberDisplay("#total-projects");
-	var netDonations = dc.numberDisplay("#net-donations");
-	var stateDonations = dc.barChart("#state-donations");
-	var bubblechart = dc.bubbleChart("#bubble-chart");
-	//var bubblechart = dc.bubbleOverlay("#bubble-chart");
+	var fundingChart = dc.pieChart("#funding-chart");
+	var sourceBarChart = dc.rowChart("#source-bar-chart");
+	var totalSpentChart = dc.numberDisplay("#total-projects");
+	var bubbleChart = dc.bubbleChart("#bubble-chart");
 	var dataTable = dc.dataTable("#dc-table-graph");
-	var usChart = dc.geoChoroplethChart("#us-chart");
+	var arMapChart = dc.geoChoroplethChart("#ar-states");
 	var institutionChart = dc.rowChart("#institution-chart");
 
-
-
-
-  	
-
-
+	// Define menus
     cbox = dc.selectMenu('#menuselectyear')
            .dimension(year)
            .group(yearGroup);
@@ -170,28 +79,47 @@ console.log(maxDate);
 	        .group(all);
 
 
-	// total proposed
-	totalProjects
+	// We need to wrap the variables for the bubblechart. We apply some ñapas
+	// in order to match the huge amounts and fit a chachi piruli plot
+	var budgetReduced = type.group().reduce(
+        function(d, v) {
+            d.x += 0.11;
+            d.sum += v.total_price_excluding_optional_support*0.0000001; 
+            d.col =  v.school_metro;
+            d.y += Math.log(v.total_price_excluding_optional_support);
+            return d;
+        },
+        function(d, v) {
+            d.x -= 0.11;
+            d.sum -= v.total_price_excluding_optional_support*0.0000001;                    
+            d.y -= Math.log(v.total_price_excluding_optional_support);
+            d.col =  v.school_metro;
+            return d;
+        },
+        function() {
+            return {
+                x:0,y:0,sum:0, col:0
+            };
+        }
+        );
+
+	////////////
+	// CHARTS //
+	////////////
+
+	// total spent
+	totalSpentChart
 		.formatNumber(d3.format("d"))
 		.valueAccessor(function(d){return d; })
-		.group(presupuestoP)
+		.group(budgetGroup)
 		.formatNumber(d3.format("$.3s"));
 
-
-	// Total paid budget
-	netDonations
-		.formatNumber(d3.format("d"))
-		.valueAccessor(function(d){return d; })
-		.group(netTotalDonations)
-		.formatNumber(d3.format("$.3s"));
-
-	//
+	// Time line chart
 	dateChart
-		//.width(600)
 		.height(220)
 		.margins({top: 10, right: 50, bottom: 30, left: 50})
 		.dimension(datePosted)
-		.group(projectsByDate)
+		.group(groupByDate)
 		.renderArea(true)
 		.transitionDuration(500)
 		.x(d3.time.scale().domain([minDate, maxDate]))
@@ -201,127 +129,57 @@ console.log(maxDate);
 		.xAxisLabel("Year")
 		.yAxis().ticks(6);
 
-	resourceTypeChart
-        //.width(300)
-        .height(300)
-        .dimension(presupuesto)
-        .group(fuenteGroup)
-        .elasticX(true)
-        .xAxis().ticks(5);
 
-     // Donation Count by Poverty Level
-	povertyLevelChart
-		//.width(300)
+	// Source barchart
+	sourceBarChart
 		.height(300)
-        .dimension(presupuesto)
-        .group(fuenteGroup)
+        .dimension(budgetMoney)
+        .group(sourceGroup)
         .ordering(function(d) { return -d.value })
         .xAxis().ticks(4);
 
-    // INSTITUTION CHART
+    // Institution barchart
 	institutionChart
-		//.width(300)
 		.height(1000)
-        .dimension(presupuesto)
+        .dimension(budgetMoney)
         .group(institutionGroup)
         .rowsCap(20)
         .ordinalColors(['lightblue'])
         .ordering(function(d) { return -d.value })
         .xAxis().ticks(4);
 
-
-    // donation count per grade
-	gradeLevelChart
-		//.width(300)
-		.height(300)
-        .dimension(presupuesto)
-        .group(fuenteGroup)
-        .xAxis().ticks(4);
-
   
-  // PIE CHART
-	fundingStatusChart
+  	// Funding piechart
+	fundingChart
 		.height(300)
-		//.width(350)
 		.radius(140)
 		.innerRadius(40)
 		.transitionDuration(1000)
-		.dimension(presupuesto)
+		.dimension(budgetMoney)
 		.group(caracterGroup)
 		.legend(dc.legend());
 
-
-    stateDonations
-    	.width(800)
-        .height(220)
-        .transitionDuration(1000)
-        .dimension(state)
-        .group(totalDonationsState)
-        .margins({top: 10, right: 50, bottom: 30, left: 50})
-        .centerBar(false)
-        .gap(5)
-        .elasticY(true)
-        .x(d3.scale.ordinal().domain(state))
-        .xUnits(dc.units.ordinal)
-        .renderHorizontalGridLines(true)
-        .renderVerticalGridLines(true)
-        .ordering(function(d){return d.value;})
-        .yAxis().tickFormat(d3.format("s"));
-
-	var presupuestoPor = schoolDistrict.group().reduce(
-                function(d, v) {
-                    d.x += 0.11;
-                    d.sum += v.total_price_excluding_optional_support*0.0000001; 
-                    d.col =  v.school_metro;
-                    d.y += Math.log(v.total_price_excluding_optional_support);
-                    //console.log(d.col);
-                    return d;
-                },
-                function(d, v) {
-                    d.x -= 0.11;
-                    d.sum -= v.total_price_excluding_optional_support*0.0000001;                    
-                    d.y -= Math.log(v.total_price_excluding_optional_support);
-                    d.col =  v.school_metro;
-                    //console.log(d.col);
-                    return d;
-                },
-                function() {
-                    return {
-                        x:0,y:0,sum:0, col:0
-                    };
-                }
-        );
-		
-
-    bubblechart
+	// Bubblechart
+    bubbleChart
 		.width(1000)
         .height(600)
-        //UNIDAD
-    	.dimension(schoolDistrict) // set dimension
-        .group(presupuestoPor)
-        // Color
+    	.dimension(type) 
+        .group(budgetReduced)
 		.colorAccessor(function(d) {
             return d.value.col;
         })  
-        // X
+        // X axis
         .keyAccessor(function(d) {return d.value.col;})
-        // Y
+        // Y axis
         .valueAccessor(function(d) {return d.value.y;})
-        // closure used to retrieve radius value from multi-value group
-        //.radiusValueAccessor(function(d) {return d.value;})
         .radiusValueAccessor(function(d) {return d.value.sum;})
-        // set x scale
-        //.x(d3.scale.linear().domain([-10, 140]))
-        // set y scale
-        //.y(d3.scale.linear().domain([-300, 400]))
-
         .x(d3.scale.linear().domain([-100, 1000]))
         .y(d3.scale.linear().domain([-100, 1000]))
         // set radius scale
         .r(d3.scale.linear().domain([0, 300000]))
 		// (optional) render horizontal grid lines, :default=false
         .renderHorizontalGridLines(false)
-            // (optional) render vertical grid lines, :default=false
+        // (optional) render vertical grid lines, :default=false
         .renderVerticalGridLines(false) 
         .renderLabel(true)
         .renderTitle(false)
@@ -332,12 +190,8 @@ console.log(maxDate);
         .maxBubbleRelativeSize(0.4)
         .margins({top: 10, right: 50, bottom: 30, left: 40});
 
-	
-
- 
-
-
-	usChart.width(1000)
+    // map
+	arMapChart.width(1000)
 		.height(600)
 		.dimension(state)
 		.group(totalDonationsByState)
@@ -346,11 +200,6 @@ console.log(maxDate);
 		.overlayGeoJson(statesJson["features"], "state", function (d) {
 			return d.properties.name;
 		})
-
-		//.projection(d3.geo.albersUsa()
-    	//			.scale(600)
-    	//			.translate([340, 150])
-    	//			) 
     	.projection(d3.geo.transverseMercator()
                      .center([2.5, -38.5])
                      .rotate([66, 0])
@@ -363,12 +212,7 @@ console.log(maxDate);
 					+ "Total Donations: " + Math.round(p["value"]) + " $";
 		});
   
-
-    
-
-
-
-	// Table of earthquake data
+	// Table of atomic data
 	  dataTable.width(1000).height(400)
 	    .dimension(datePosted)
 		.group(function(d) { return ""
@@ -382,18 +226,5 @@ console.log(maxDate);
 	    .sortBy(function(d){ return d.total_price_excluding_optional_support; })
 	    .order(d3.descending);
 
-
-
-		
-		
-		
-		
-		
-		
-
-
-
-
     dc.renderAll();
-
 };
